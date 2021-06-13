124af1efc0342feac17c88186ffbd8787548efec
Jeremy Kepf <jeremy1975@gmail.com>
2021-06-13 10:10:14 -0400

    api2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://api.gemini-collectibles.com`;
+export const prodEndpoint = `https://api.gemini-collectibles.com/api/graphql`;
 export const perPage = 8;
 export const inventoryPerPage = 100;
