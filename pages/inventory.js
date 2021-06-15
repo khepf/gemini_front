75cc8f8c17ad7fa3181be8d93c18512a9aee209d
Jeremy Kepf <jeremy1975@gmail.com>
2021-06-15 18:22:35 -0400

    warning fix after build fail14

---
+++ pages/inventory.js
@@ -0,0 +1,27 @@
+import CreateBaseballCardButton from "../components/CreatBaseballCardButton";
+
+import { useUser } from '../components/User';
+import PleaseSignIn from "../components/PleaseSignIn";
+
+
+export default function InventoryPage() {
+    return (<div>
+     {user && user.role.name == "Admin"  && (
+       <>
+      <div><CreateBaseballCardButton /></div>
+      <h4>Hello Admin</h4>
+      </>
+     )}
+     {user && user.role.name == "Seller"  && (
+       <>
+       <div><CreateBaseballCardButton /></div>
+       <h4>Hello Seller</h4>
+      </>
+     )}
+     {!user && (
+       <h5>You do not have access to this page, silly goose.</h5>
+     )}
+
+  </div>
+  )
+}
