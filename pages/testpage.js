8157c675c06cf937f3d842cb3e07679c3c71543e
Jeremy Kepf <jeremy1975@gmail.com>
2021-06-15 18:12:14 -0400

    warning fix after build fail12

---
+++ pages/testpage.js
@@ -0,0 +1,11 @@
+import CreateBaseballCardButton from "../components/CreatBaseballCardButton";
+
+
+export default function TestPage() {
+    return (<div>
+<h4>helllo inventory page</h4>
+<CreateBaseballCardButton />
+
+  </div>
+  )
+}
